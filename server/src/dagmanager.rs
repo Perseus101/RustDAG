b1339e921000b51c0f2ca4b983dd5733fc78d2df
Colin Moore <colin@moore.one>
2018-07-15 14:02:08 -0400

    DAGManager ignores known incoming transactions

--- server/src/dagmanager.rs
+++ server/src/dagmanager.rs
@@ -36,6 +36,10 @@ impl DAGManager {
     }
 
     pub fn add_transaction(&self, transaction: Transaction) -> bool {
+        // Ignore any already known transactions
+        if self.dag.read().unwrap().get_transaction(transaction.get_hash()) != None {
+            return false;
+        }
         if self.dag.write().unwrap().add_transaction(&transaction) {
             self.peers.read().unwrap().map_peers(|peer| {
                 peer.post_transaction(&transaction)
