f041b8bfd97c14ac94089a9d4ded9121694eb679
Colin Moore <colin@moore.one>
2019-03-02 03:44:39 -0500

    Add MPT merge operation

--- lib/src/dag/storage/mpt.rs
+++ lib/src/dag/storage/mpt.rs
@@ -15,7 +15,7 @@ fn get_bottom_nibble(val: u64) -> u8 {
     (val & 0x0000_0000_0000_000F) as u8
 }
 
-#[derive(Clone, Hash, Debug)]
+#[derive(Clone, Hash, PartialEq, Debug)]
 pub struct PointerNode {
     x_0: Option<u64>,
     x_1: Option<u64>,
@@ -60,7 +60,11 @@ impl Default for PointerNode {
 
 impl PointerNode {
     fn get_next_hash(&self, k: u64) -> Option<u64> {
-        match get_top_nibble(k) {
+        self.get(get_top_nibble(k))
+    }
+
+    fn get(&self, index: u8) -> Option<u64> {
+        match index {
             0x0 => self.x_0,
             0x1 => self.x_1,
             0x2 => self.x_2,
@@ -104,13 +108,42 @@ impl PointerNode {
     }
 }
 
-#[derive(Clone, Hash, Debug)]
-pub enum Node<T: Hash + Clone + Debug> {
+struct PointerNodeIterator<'a> {
+    index: u8,
+    node: &'a PointerNode
+}
+
+impl<'a> PointerNodeIterator<'a> {
+    fn new(node: &'a PointerNode) -> Self {
+        PointerNodeIterator {
+            index: 0,
+            node: node
+        }
+    }
+}
+
+impl<'a> Iterator for PointerNodeIterator<'a> {
+    type Item = Option<u64>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index == 16 {
+            return None;
+        }
+
+        let res = self.node.get(self.index);
+        self.index += 1;
+
+        Some(res)
+    }
+}
+
+#[derive(Clone, Hash, PartialEq, Debug)]
+pub enum Node<T: Hash + PartialEq + Clone + Debug> {
     BranchNode(PointerNode),
     LeafNode(T)
 }
 
-impl<T: Hash + Clone + Debug> Node<T> {
+impl<T: Hash + PartialEq + Clone + Debug> Node<T> {
     pub fn get_hash(&self) -> u64 {
         let mut s = Sha3Hasher::new();
         self.hash(&mut s);
@@ -118,42 +151,38 @@ impl<T: Hash + Clone + Debug> Node<T> {
     }
 }
 
-pub struct NodeUpdates<T: Hash + Clone + Debug> {
+#[derive(PartialEq, Debug)]
+pub struct NodeUpdates<T: Hash + PartialEq + Clone + Debug> {
     root: Node<T>,
-    leaf: Node<T>,
     branches: Vec<Node<T>>,
 }
 
-impl<T: Hash + Clone + Debug> IntoIterator for NodeUpdates<T> {
+impl<T: Hash + PartialEq + Clone + Debug> IntoIterator for NodeUpdates<T> {
     type Item = Node<T>;
     type IntoIter = ::std::vec::IntoIter<Node<T>>;
 
     fn into_iter(self) -> Self::IntoIter {
         let root = self.root;
-        let leaf = self.leaf;
         let mut vec = self.branches;
         vec.push(root);
-        vec.push(leaf);
         vec.into_iter()
     }
 }
 
-impl<T: Hash + Clone + Debug> NodeUpdates<T> {
-    fn new(root: Node<T>, leaf: Node<T>, branches: Vec<Node<T>>) -> Self {
+impl<T: Hash + PartialEq + Clone + Debug> NodeUpdates<T> {
+    fn new(root: Node<T>, branches: Vec<Node<T>>) -> Self {
         NodeUpdates {
             root,
-            leaf,
             branches,
         }
     }
 }
 
-pub struct MerklePatriciaTree<T: Hash + Clone + Debug> {
+pub struct MerklePatriciaTree<T: Hash + PartialEq + Clone + Debug> {
     nodes: HashMap<u64, Node<T>>,
-    root: u64
 }
 
-impl<T: Hash + Clone + Debug> MerklePatriciaTree<T> {
+impl<T: Hash + PartialEq + Clone + Debug> MerklePatriciaTree<T> {
     pub fn new() -> Self {
         let mut nodes = HashMap::new();
         let root = Node::BranchNode(PointerNode::default());
@@ -161,12 +190,15 @@ impl<T: Hash + Clone + Debug> MerklePatriciaTree<T> {
         nodes.insert(hash, root);
         MerklePatriciaTree {
             nodes: nodes,
-            root: hash
         }
     }
 
-    pub fn get(&self, mut k: u64) -> Option<&T> {
-        let mut node = self.nodes.get(&self.root);
+    pub fn default_root() -> u64 {
+        Node::BranchNode::<T>(PointerNode::default()).get_hash()
+    }
+
+    pub fn get(&self, root: u64, mut k: u64) -> Option<&T> {
+        let mut node = self.nodes.get(&root);
         // 16 branch nodes + 1 leaf node
         for _ in 0..17 {
             match node {
@@ -181,16 +213,16 @@ impl<T: Hash + Clone + Debug> MerklePatriciaTree<T> {
                 Some(Node::LeafNode(value)) => {
                     return Some(value);
                 }
-                _ => panic!("Malformed MerklePatriciaTree node(s)"),
+                _ => panic!("get: Malformed MerklePatriciaTree node(s)"),
             }
             k <<= 4;
         }
         None
     }
 
-    pub fn try_set(&self, k: u64, v: T) -> NodeUpdates<T> {
+    pub fn try_set(&self, root: u64, k: u64, v: T) -> NodeUpdates<T> {
         let mut new_nodes = Vec::new();
-        let root_node = self.nodes.get(&self.root)
+        let root_node = self.nodes.get(&root)
             .expect("Root node does not exist");
         {
             let mut node = root_node;
@@ -239,26 +271,102 @@ impl<T: Hash + Clone + Debug> MerklePatriciaTree<T> {
                 hash = node.get_hash();
                 key >>= 4;
             }
+            new_nodes.push(leaf_node);
             let mut new_root = root_node.clone();
             if let Node::BranchNode(ref mut pointers) = new_root {
                 pointers.set_hash(get_bottom_nibble(key), hash);
             }
-            NodeUpdates::new(new_root, leaf_node, new_nodes)
+            NodeUpdates::new(new_root, new_nodes)
         }
     }
 
     pub fn commit_set(&mut self, updates: NodeUpdates<T>) {
-        self.root = updates.root.get_hash();
         for node in updates.into_iter() {
             self.nodes.insert(node.get_hash(), node);
         }
     }
 
-    pub fn set(&mut self, k: u64, v: T) {
-        let updates = {
-            self.try_set(k, v)
-        };
-        self.commit_set(updates);
+    pub fn try_merge(&self, hash_a: u64, hash_b: u64, hash_ref: u64)
+            -> Option<NodeUpdates<T>> {
+        if hash_a == hash_b {
+            return Some(NodeUpdates::new(self.nodes.get(&hash_a)
+                .expect("Root node does not exist").clone(), Vec::new()))
+        }
+        let root_a = self.nodes.get(&hash_a)
+            .expect("Root node does not exist");
+        let root_b = self.nodes.get(&hash_b)
+            .expect("Root node does not exist");
+        let root_ref = self.nodes.get(&hash_ref)
+            .expect("Root node does not exist");
+
+        if let (Node::LeafNode(a_val), Node::LeafNode(b_val),
+                    Node::LeafNode(ref_val)) = (root_a, root_b, root_ref) {
+            if a_val != ref_val && b_val != ref_val {
+                // Invalid merge
+                None
+            }
+            else if a_val != ref_val {
+                Some(NodeUpdates::new(Node::LeafNode(a_val.clone()), Vec::new()))
+            }
+            else {
+                Some(NodeUpdates::new(Node::LeafNode(b_val.clone()), Vec::new()))
+            }
+        }
+        else if let (Node::BranchNode(a_pointers), Node::BranchNode(b_pointers),
+                Node::BranchNode(ref_pointers)) = (root_a, root_b, root_ref) {
+            let mut new_nodes = Vec::new();
+            let mut new_ptr = ref_pointers.clone();
+
+            let a_iter = PointerNodeIterator::new(a_pointers);
+            let b_iter = PointerNodeIterator::new(b_pointers);
+            let ref_iter = PointerNodeIterator::new(ref_pointers);
+
+            for ((i, ref_ptr), (a_ptr, b_ptr)) in ref_iter.enumerate().zip(a_iter.zip(b_iter)) {
+                if a_ptr != b_ptr {
+                    match (a_ptr, b_ptr, ref_ptr) {
+                        (Some(a), Some(b), Some(r)) => {
+                            // Recurse, checking valid merge for child
+                            let mut res = self.try_merge(a, b, r);
+                            if let Some(child_updates) = res {
+                                // Insert child data into new_ptr and new_nodes
+                                new_ptr.set_hash(i as u8, child_updates.root.get_hash());
+                                new_nodes.extend(child_updates.into_iter());
+                            }
+                            else {
+                                // The merge is invalid
+                                return None;
+                            }
+                        },
+                        (Some(_), Some(_), None) => {
+                            // There is no way to know if a and b can be merged,
+                            // so return invalid merge
+                            return None;
+                        },
+                        (Some(child_ptr), None, None) |
+                        (None, Some(child_ptr), None) => {
+                            // Insert updated node
+                            new_ptr.set_hash(i as u8, child_ptr);
+                        },
+                        (None, _, Some(_)) |
+                        (_, None, Some(_)) => {
+                            // This is a special invalid merge, because the
+                            // chosen reference tree was incorrect
+                            return None;
+                        },
+                        (None, None, _) => {
+                            // This should be unreachable, since a_ptr and b_ptr
+                            // are not equal
+                            panic!("try_merge: a_ptr and b_ptr are unexpectedly equal");
+                        }
+                    }
+                }
+            }
+            Some(NodeUpdates::new(Node::BranchNode(new_ptr), new_nodes))
+        }
+        else {
+            // If we get here, one or more of the trees is malformed
+            panic!("try_merge: Malformed MerklePatriciaTree node(s)");
+        }
     }
 }
 
@@ -316,26 +424,76 @@ mod tests {
         assert_eq!(0xF, get_bottom_nibble(0xFFFF_FFFF_FFFF_FFFF));
     }
 
+    #[test]
+    fn test_default_root() {
+        let mpt: MerklePatriciaTree<u64> = MerklePatriciaTree::new();
+        assert_eq!(*mpt.nodes.iter().next().unwrap().0,
+            MerklePatriciaTree::<u64>::default_root());
+    }
 
     #[test]
     fn test_mpt_get_empty() {
         let mpt: MerklePatriciaTree<u64> = MerklePatriciaTree::new();
-        assert_eq!(mpt.get(0), None);
+        assert_eq!(mpt.get(MerklePatriciaTree::<u64>::default_root(), 0), None);
+    }
+
+    fn mpt_set<T: Hash + PartialEq + Clone + Debug>(mpt: &mut MerklePatriciaTree<T>, root: u64, k: u64, v: T) -> u64 {
+        let updates = {
+            mpt.try_set(root, k, v)
+        };
+        let new_root = updates.root.get_hash();
+        mpt.commit_set(updates);
+        new_root
     }
 
     #[test]
     fn test_mpt_get_set() {
         let mut mpt: MerklePatriciaTree<u64> = MerklePatriciaTree::new();
-        mpt.set(0x1234_5678_9ABC_DEF0, 0);
-        assert_eq!(mpt.get(0x1234_5678_9ABC_DEF0), Some(&0));
+        let mut root = MerklePatriciaTree::<u64>::default_root();
+        root = mpt_set(&mut mpt, root, 0x1234_5678_9ABC_DEF0, 0);
+        assert_eq!(mpt.get(root, 0x1234_5678_9ABC_DEF0), Some(&0));
 
-        mpt.set(0, 0);
-        assert_eq!(mpt.get(0), Some(&0));
-        mpt.set(1, 0);
-        assert_eq!(mpt.get(1), Some(&0));
-        mpt.set(2, 100);
-        assert_eq!(mpt.get(2), Some(&100));
-        mpt.set(u64::max_value(), u64::max_value());
-        assert_eq!(mpt.get(u64::max_value()), Some(&u64::max_value()));
+        root = mpt_set(&mut mpt, root, 0, 0);
+        assert_eq!(mpt.get(root, 0), Some(&0));
+        root = mpt_set(&mut mpt, root, 1, 0);
+        assert_eq!(mpt.get(root, 1), Some(&0));
+        root = mpt_set(&mut mpt, root, 2, 100);
+        assert_eq!(mpt.get(root, 2), Some(&100));
+        root = mpt_set(&mut mpt, root, u64::max_value(), u64::max_value());
+        assert_eq!(mpt.get(root, u64::max_value()), Some(&u64::max_value()));
+        for i in 8..64 {
+            root = mpt_set(&mut mpt, root, i, i);
+        }
+        for i in 8..64 {
+            assert_eq!(mpt.get(root, i), Some(&i));
+        }
+    }
+
+    #[test]
+    fn test_mpt_merge() {
+        let mut mpt: MerklePatriciaTree<u64> = MerklePatriciaTree::new();
+        let mut root = MerklePatriciaTree::<u64>::default_root();
+        root = mpt_set(&mut mpt, root, 0, 0);
+        assert_eq!(mpt.get(root, 0), Some(&0));
+
+        let mut root_a;
+        let mut root_b;
+
+        // Invalid merge, key 1 modified in both
+        root_a = mpt_set(&mut mpt, root, 1, 1);
+        root_b = mpt_set(&mut mpt, root, 1, 2);
+
+        assert_eq!(mpt.try_merge(root_a, root_b, root), None);
+
+        // Valid merges, different keys
+        root_a = mpt_set(&mut mpt, root, 1, 1);
+        for i in 2..128 {
+            root_b = mpt_set(&mut mpt, root, i, i);
+
+            let updates = mpt.try_merge(root_a, root_b, root).unwrap();
+            let new_root = updates.root.get_hash();
+            mpt.commit_set(updates);
+            assert_eq!(mpt.get(new_root, i), Some(&i));
+        }
     }
 }
